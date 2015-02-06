6224b8603ea725efaa1bb52781c5651ad5abfeca
danosted <danosted@gmail.com>
2015-02-07 00:34:50 +0100

    no message

--- NGJ2015/Assets/src/Managers/EnemyManager.cs
+++ NGJ2015/Assets/src/Managers/EnemyManager.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Backend.DataAccess.BaseClasses;
+using Assets.src.Common;
 using UnityEngine;
 
 namespace Assets.src.Managers
 {
-    public class EnemyManager : MonoBehaviour
+    public class EnemyManager : ManagerBase
     {
 // DO STUFF
-
+        public GameObject GetNewEnemyFromType(Enumerations.EnemyType enemyType)
+        {
+            Debug.Log(string.Format("Fetching object with type '{0}'.", enemyType));
+            var GO = PrefabPool.Find(x => x.GetComponent(enemyType.ToString()) != null);
+            if (GO == null)
+            {
+                var msg = string.Format("No object found with type '{0}'.", enemyType);
+                Debug.LogError(msg, gameObject);
+            }
+            var resultGO = GameObject.Instantiate(GO) as GameObject;
+            ActiveObjects.Add(resultGO.gameObject);
+            resultGO.transform.parent = transform;
+        }
 
     }
 }
