6f1deab267dfa032167760354c5985ec2e391dcd
Patrik <the__pal@hotmail.com>
2015-02-07 13:00:08 +0100

    Weapon Manager added

---
+++ NGJ2015/Assets/src/Managers/WeaponManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.src.Managers;
+
+namespace Assets.src.Managers
+{
+    public class WeaponManager : ManagerBase
+    {
+        
+    }
+}
