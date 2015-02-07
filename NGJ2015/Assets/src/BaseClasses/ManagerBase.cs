a7b61c6d0f52c49313a9619eb883085b98248b20
danosted <danosted@gmail.com>
2015-02-07 01:28:04 +0100

    sup

--- NGJ2015/Assets/src/BaseClasses/ManagerBase.cs
+++ NGJ2015/Assets/src/BaseClasses/ManagerBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace Assets.Backend.DataAccess.BaseClasses
+namespace Assets.src.DataAccess.BaseClasses
 {
     public class ManagerBase : MonoBehaviour
     {
---
+++ NGJ2015/Assets/src/Managers/EventHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.src.Managers
+{
+    public class EventHandler
+    {
+        public delegate void OnDamageTakenDelegate();
+        public event OnDamageTakenDelegate OnDamageTaken;
+
+        public delegate void OnPunchHitDelegate();
+        public event OnPunchHitDelegate OnPunchHit;
+
+        public delegate void OnPunchMissDelegate();
+        public event OnPunchMissDelegate OnPunchMiss;
+
+        public delegate void OnWaveStartedDelegate();
+        public event OnWaveStartedDelegate OnWaveStarted;
+
+        public void WaveStarted()
+        {
+            if (OnWaveStarted != null)
+            {
+                OnWaveStarted();
+            }
+        }
+
+        public void PunchHit()
+        {
+            if (OnPunchHit != null)
+            {
+                OnPunchHit();
+            }
+        }
+
+        public void PunchMiss()
+        {
+            if (OnPunchMiss != null)
+            {
+                OnPunchMiss();
+            }
+        }
+    }
+}
