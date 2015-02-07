824d97f59982096ef15897461d9103689f94d198
unknown <lvj@T440SW74X4CR.nclan.netcompany.dk>
2015-02-07 18:04:06 +0100

    Moved enemy types around - DrawerEnemy started

---
+++ NGJ2015/Assets/src/Managers/Entities/ChairEnemy.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.src.Managers.Entities
+{
+    public class ChairEnemy : Enemy
+	{
+
+        public int MinFramesToKeepAStrategy = 30;
+
+        protected int _strategyFrameNum = 0;
+        private MonsterStrategy _previousStrategy;
+
+        public float NearbyMonstersDist = 5;
+        public float ChargeDist = 5;
+        public int SwarmThreshold = 5;
+        public int SpreadThreshold = 10;
+        public int minEnemyDistance = 2;
+
+        private enum MonsterStrategy
+        {
+            Spread,
+            Swarm,
+            Attack
+        }
+
+        public void Update()
+        {
+        }
+
+        public void FixedUpdate()
+        {
+
+		    var strategy = ChooseStrategy();
+
+		    switch (strategy)
+		    {
+                case MonsterStrategy.Attack:
+		            ExecuteAttackStrategy();
+                    break;
+
+                case MonsterStrategy.Spread:
+                    ExecuteSpreadStrategy();
+                    break;
+
+                case MonsterStrategy.Swarm:
+                    ExecuteSwarmStrategy();
+                    break;
+
+		    }
+
+            transform.position += KeepEnemyDistance();
+
+		    if (_target)
+		    {
+                if (Vector3.Magnitude(transform.position - _target.transform.position) < _range)
+                {
+                    _target.TakeDamage(_damage);
+                }
+		    }
+		    
+		}
+
+        private MonsterStrategy ChooseStrategy()
+        {
+            _strategyFrameNum++;
+            if (_strategyFrameNum < MinFramesToKeepAStrategy)
+            {
+                return _previousStrategy;
+            }
+            _target = GetNearestTarget(_targets, transform.position).GetComponent<CharacterBase>();
+
+            _nearbyMonsters =
+                ManagerCollection.Instance.EnemyManager.GetActiveMonsters()
+                    .Select(
+                        m => new MonsterDist { Dist = (m.transform.position - transform.position), Monster = m })
+                    .Where(m => m.Dist.magnitude < NearbyMonstersDist)
+                    .ToList();
+            
+
+            if ((_target.transform.position - transform.position).magnitude < ChargeDist)
+            {
+                _previousStrategy = MonsterStrategy.Attack;
+                return _previousStrategy;
+            }
+
+            
+            if (_nearbyMonsters.Count >= SwarmThreshold && _nearbyMonsters.Count < SpreadThreshold)
+            {
+                _previousStrategy = MonsterStrategy.Swarm;
+                return _previousStrategy;
+            }
+
+            if (_nearbyMonsters.Count >= SpreadThreshold)
+            {
+                _previousStrategy =  MonsterStrategy.Spread;
+                return _previousStrategy;
+            }
+
+            _previousStrategy = MonsterStrategy.Attack;
+            return _previousStrategy;
+        }
+
+        private void ExecuteAttackStrategy()
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.magenta;
+            if (_target)
+            {
+                //var msg = string.Format("Enemy {0} is moving towards {1}.", gameObject, _target);
+                //Debug.Log(msg, gameObject);
+                transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, Time.fixedDeltaTime * _speed);
+            }
+        }
+
+
+        private void ExecuteSpreadStrategy()
+        {
+            //var msg = string.Format("Enemy {0} is spreading towards {1}.", gameObject, _target);
+            //Debug.Log(msg, gameObject);
+
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.red;
+
+            var centerPosition = new Vector3();
+
+            foreach (var pos in _nearbyMonsters.Select(m => m.Monster.transform.position))
+            {
+                centerPosition += pos;
+            }
+
+            centerPosition /= _nearbyMonsters.Count;
+
+            var direction = transform.position - centerPosition;
+
+            if (_target)
+            {
+                direction *= 2;
+                direction += _target.transform.position - transform.position;
+                direction /= 3;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, Time.fixedDeltaTime * _speed);
+        }
+
+        private void ExecuteSwarmStrategy()
+        {
+            //var msg = string.Format("Enemy {0} is swarming towards {1}.", gameObject, _target);
+            //Debug.Log(msg, gameObject);
+
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+
+            var finalDirection = new Vector3();
+            var positions = _nearbyMonsters.Select(m => m.Monster.transform.position);
+            foreach (var pos in positions)
+            {
+                finalDirection += pos;
+            }
+
+            finalDirection /= _nearbyMonsters.Count;
+            if (_target)
+            {
+                finalDirection += _target.transform.position;
+                finalDirection /= 2;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, finalDirection, Time.fixedDeltaTime * _speed);
+        }
+
+        public override void Die()
+        {
+            Debug.Log("Enemy die");
+            base.Die();
+            ManagerCollection.Instance.EnemyManager.PoolEnemyObject(gameObject);
+        }
+
+    }
+}
---
+++ NGJ2015/Assets/src/Managers/Entities/DrawerEnemy.cs
@@ -0,0 +1,144 @@
+﻿using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.src.Managers.Entities
+{
+    public class DrawerEnemy : Enemy
+	{
+		
+        public float MoveCloserDist = 6;
+        public float MoveAwayDist = 4;
+
+        private MonsterStrategy _previousStrategy;
+
+        private enum MonsterStrategy
+        {
+            MoveCloser,
+            Idle,
+            MoveAway
+        }
+
+
+        public void Update()
+        {
+        }
+
+        public void FixedUpdate()
+        {
+
+		    var strategy = ChooseStrategy();
+
+		    switch (strategy)
+		    {
+                case MonsterStrategy.Idle:
+		            ExecuteIdleStrategy();
+                    break;
+
+                case MonsterStrategy.MoveAway:
+                    ExecuteMoveAwayStrategy();
+                    break;
+
+                case MonsterStrategy.MoveCloser:
+                    ExecuteMoveCloserStrategy();
+                    break;
+
+		    }
+
+            transform.position += KeepEnemyDistance();
+
+		    if (_target)
+		    {
+                if (Vector3.Magnitude(transform.position - _target.transform.position) < _range)
+                {
+                    _target.TakeDamage(_damage);
+                }
+		    }
+		    
+		}
+
+        private MonsterStrategy ChooseStrategy()
+        {
+            _strategyFrameNum++;
+            if (_strategyFrameNum < MinFramesToKeepAStrategy)
+            {
+                return _previousStrategy;
+            }
+
+            _target = GetNearestTarget(_targets, transform.position).GetComponent<CharacterBase>();
+
+            _nearbyMonsters =
+                ManagerCollection.Instance.EnemyManager.GetActiveMonsters()
+                    .Select(
+                        m => new MonsterDist { Dist = (m.transform.position - transform.position), Monster = m })
+                    .Where(m => m.Dist.magnitude < NearbyMonstersDist)
+                    .ToList();
+
+            var targetDistance = (_target.transform.position - transform.position).magnitude;
+            
+            if (targetDistance >= MoveCloserDist)
+            {
+                _previousStrategy = MonsterStrategy.MoveCloser;
+                return _previousStrategy;
+            }
+
+            if (targetDistance < MoveAwayDist)
+            {
+                _previousStrategy = MonsterStrategy.MoveAway;
+                return _previousStrategy;
+            }
+
+            _previousStrategy = MonsterStrategy.Idle;
+            return _previousStrategy;
+        }
+
+        private void ExecuteIdleStrategy()
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.magenta;
+            if (_target)
+            {
+                var r = new Random();
+                transform.position += (new Vector3
+                {
+                    x = (float) r.NextDouble(),
+                    y = (float) r.NextDouble(),
+                    z = 0
+                })/(Time.fixedDeltaTime * _speed);
+            }
+        }
+
+
+        private void ExecuteMoveAwayStrategy()
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.red;
+
+
+            if (_target != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, transform.position + (transform.position - _target.transform.position), Time.fixedDeltaTime * _speed);
+            }
+            
+        }
+
+        private void ExecuteMoveCloserStrategy()
+        {
+            //var msg = string.Format("Enemy {0} is spreading towards {1}.", gameObject, _target);
+            //Debug.Log(msg, gameObject);
+
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.red;
+
+            if (_target != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, transform.position + (_target.transform.position - transform.position), Time.fixedDeltaTime * _speed);
+            }
+        }
+
+
+        public override void Die()
+        {
+            Debug.Log("Enemy die");
+            base.Die();
+            ManagerCollection.Instance.EnemyManager.PoolEnemyObject(gameObject);
+        }
+    }
+}
