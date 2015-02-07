84b6f20435486c77d06f0a0bf7ebe2a8ff1de9a1
unknown <lvj@T440SW74X4CR.nclan.netcompany.dk>
2015-02-07 13:45:40 +0100

    Removed debug logs

--- NGJ2015/Assets/src/Logic/GameLogic.cs
+++ NGJ2015/Assets/src/Logic/GameLogic.cs
@@ -16,8 +16,8 @@ public class GameLogic : MonoBehaviour
         manCol.EventManager.GameStarted();
         var player1 = manCol.PlayerManager.GetNewPlayerFromType(Enumerations.PlayerType.Player);
         var p1 = player1.GetComponent(Enumerations.PlayerType.Player.ToString()) as Player;
-        var player2 = manCol.PlayerManager.GetNewPlayerFromType(Enumerations.PlayerType.Player);
-        var p2 = player2.GetComponent(Enumerations.PlayerType.Player.ToString()) as Player;
+        //var player2 = manCol.PlayerManager.GetNewPlayerFromType(Enumerations.PlayerType.Player);
+        //var p2 = player2.GetComponent(Enumerations.PlayerType.Player.ToString()) as Player;
 		p1.Initialize(10,10,10,5);
 		var enemyLogic = EnemyLogic.Instance;
     }
